2cbcf18f202b2c2a2cec7ca43f57212e54267b88
Diego <diegosantos4957@gmail.com>
2025-11-04 15:43:35 -0300

    infra: start module and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrdersModule } from './modules/orders/orders.module';
+import { UsersModule } from './modules/users/users.module';
+import { ProductsModule } from './modules/products/products.module';
 
 @Module({
-  imports: [],
+  imports: [OrdersModule, UsersModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
