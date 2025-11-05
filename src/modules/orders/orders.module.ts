56f4771d32a4b8c1c21348d95f31d31f8e9059f9
Diego <diegosantos4957@gmail.com>
2025-11-04 21:36:19 -0300

    infra: initial repository

--- src/modules/orders/orders.module.ts
+++ src/modules/orders/orders.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { OrdersController } from './controllers/orders.controller';
 import { OrdersService } from './services/orders.service';
+import { OrderRepository } from './repositories/order.repository';
 
 @Module({
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [OrdersService, OrderRepository],
+  exports: [OrderRepository],
 })
 export class OrdersModule {}
--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './controllers/products.controller';
 import { ProductsService } from './services/products.service';
+import { ProductsRepository } from './repositories/product.repository';
 
 @Module({
   controllers: [ProductsController],
-  providers: [ProductsService]
+  providers: [ProductsService, ProductsRepository],
+  exports: [ProductsRepository],
 })
 export class ProductsModule {}
