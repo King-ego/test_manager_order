d834745679b951ca806ee4af3d17984ac302ddc7
Diego <diegosantos4957@gmail.com>
2025-11-05 23:44:42 -0300

    feat: update crud product

--- src/modules/products/controllers/products.controller.ts
+++ src/modules/products/controllers/products.controller.ts
@@ -1,12 +1,18 @@
-import {Body, Controller, Post} from '@nestjs/common';
+import {Body, Controller, Get, Post, Param, Delete} from '@nestjs/common';
 import {CreateProductDto} from "../dto/create-product.dto";
 import {CreateProductsService} from "../services/create-product/create-products.service";
 import {Product} from "../../../../prisma/generated/mysql/client";
+import {FindProductService} from "../services/find-product/find-product.service";
+import {DeleteProductService} from "../services/delete-product/delete-product.service";
+import {ListProductService} from "../services/list-product/list-product.service";
 
 @Controller('products')
 export class ProductsController {
     constructor(
         private readonly createProductService: CreateProductsService,
+        private readonly findProductService: FindProductService,
+        private readonly deleteProductService: DeleteProductService,
+        private readonly listProductService: ListProductService,
     ) {}
 
     @Post("/")
@@ -14,5 +20,22 @@ export class ProductsController {
         const { name, price, stock } = body;
 
         return this.createProductService.execute({ name, price, stock });
+    }
+
+    @Get("/:productId")
+    public async findProduct(@Param("productId") productId: string): Promise<Product | null> {
+        return this.findProductService.execute(productId);
+    }
+
+    @Get("/")
+    public async listProducts(): Promise<Product[]> {
+        return this.listProductService.execute();
+    }
+
+    @Delete("/:productId")
+    public async deleteProduct(@Param("productId") productId: string): Promise<string> {
+        await this.deleteProductService.execute(productId);
+
+        return "Product deleted successfully";
     }
 }
--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -3,10 +3,12 @@ import { ProductsController } from './controllers/products.controller';
 import { CreateProductsService } from './services/create-product/create-products.service';
 import { ProductsRepository } from './repositories/product.repository';
 import { FindProductService } from './services/find-product/find-product.service';
+import { DeleteProductService } from './services/delete-product/delete-product.service';
+import { ListProductService } from './services/list-product/list-product.service';
 
 @Module({
   controllers: [ProductsController],
-  providers: [CreateProductsService, ProductsRepository, FindProductService],
+  providers: [CreateProductsService, ProductsRepository, FindProductService, DeleteProductService, ListProductService],
   exports: [ProductsRepository],
 })
 export class ProductsModule {}
