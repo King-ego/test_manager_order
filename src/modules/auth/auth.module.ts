85c6b8bfd61df020f577a6abae8bb915a0b5deda
Diego <diegosantos4957@gmail.com>
2025-11-08 01:08:56 -0300

    feat: enhance AuthModule with JWT strategy and guards

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,17 +4,21 @@ import { AuthService } from './services/login/auth.service';
 import {PassportModule} from "@nestjs/passport";
 import {JwtModule} from "@nestjs/jwt";
 import {jwtConfig} from "../../config/auth";
+import {UsersRepository} from "../users/repositories/users.repository";
+import {JwtAuthGuard} from "./jwt-auth.guard";
+import {JwtStrategy} from "./jwt.strategy";
 
 @Module({
     imports: [
-        PassportModule,
+        PassportModule.register({defaultStrategy: 'jwt'}),
         JwtModule.register({
             secret: jwtConfig.secret,
             signOptions: { expiresIn: jwtConfig.expiresIn },
-        }),
+        })
     ],
     controllers: [AuthController],
-    providers: [AuthService]
+    providers: [AuthService, UsersRepository, JwtStrategy, JwtAuthGuard],
+    exports: [JwtStrategy, PassportModule],
 })
 export class AuthModule {
 
