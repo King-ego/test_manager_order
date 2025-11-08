6567f97b012658c2de1b987e8918c0b4aec28c5f
Diego <diegosantos4957@gmail.com>
2025-11-07 23:34:30 -0300

    refactor: process jwt login

--- src/modules/products/controllers/products.controller.ts
+++ src/modules/products/controllers/products.controller.ts
@@ -1,10 +1,11 @@
-import {Body, Controller, Get, Post, Param, Delete} from '@nestjs/common';
+import {Body, Controller, Get, Post, Param, Delete, UseGuards} from '@nestjs/common';
 import {CreateProductDto} from "../dto/create-product.dto";
 import {CreateProductsService} from "../services/create-product/create-products.service";
 import {Product} from "../../../../prisma/generated/mysql/client";
 import {FindProductService} from "../services/find-product/find-product.service";
 import {DeleteProductService} from "../services/delete-product/delete-product.service";
 import {ListProductService} from "../services/list-product/list-product.service";
+import {JwtAuthGuard} from "../../auth/jwt-auth.guard";
 
 @Controller('products')
 export class ProductsController {
@@ -15,6 +16,7 @@ export class ProductsController {
         private readonly listProductService: ListProductService,
     ) {}
 
+    @UseGuards(JwtAuthGuard)
     @Post("/")
     public async createProduct(@Body() body: CreateProductDto): Promise<Product> {
         const { name, price, stock } = body;
@@ -22,16 +24,19 @@ export class ProductsController {
         return this.createProductService.execute({ name, price, stock });
     }
 
+    @UseGuards(JwtAuthGuard)
     @Get("/:productId")
     public async findProduct(@Param("productId") productId: string): Promise<Product | null> {
         return this.findProductService.execute(productId);
     }
 
+    @UseGuards(JwtAuthGuard)
     @Get("/")
     public async listProducts(): Promise<Product[]> {
         return this.listProductService.execute();
     }
 
+    @UseGuards(JwtAuthGuard)
     @Delete("/:productId")
     public async deleteProduct(@Param("productId") productId: string): Promise<string> {
         await this.deleteProductService.execute(productId);
