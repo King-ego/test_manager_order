9d07ffe96738da1de53b98d28c71b2cde379edc8
Diego <diegosantos4957@gmail.com>
2025-11-08 01:08:48 -0300

    feat: implement login endpoint in AuthController

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Post} from '@nestjs/common';
+import {AuthService} from "../services/login/auth.service";
+import {CreateLoginDto} from "../dto/create-login.dto";
 
-@Controller('auth')
-export class AuthController {}
+@Controller('session')
+export class AuthController {
+    constructor(
+        private readonly authService: AuthService,
+    ) {
+    }
+
+    @Post('/')
+    public async login(@Body() body: CreateLoginDto) {
+        const { email, password } = body;
+        return this.authService.execute({ email, password });
+    }
+}
