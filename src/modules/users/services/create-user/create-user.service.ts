4c0e8190a629558fb0b9ca25197db88a12d169a2
Diego <diegosantos4957@gmail.com>
2025-11-08 01:09:13 -0300

    feat: enhance CreateUserService to hash password before user creation

--- src/modules/users/services/create-user/create-user.service.ts
+++ src/modules/users/services/create-user/create-user.service.ts
@@ -1,13 +1,15 @@
 import {HttpStatus, Injectable} from '@nestjs/common';
-import { UsersRepository } from '../../repositories/users.repository';
+import {UsersRepository} from '../../repositories/users.repository';
 import {CreateUserDto} from "../../dto/create-user.dto";
 import {CustomerException} from "../../../../shared/errors/customerException";
 import {SystemLogs} from "../../../../shared/logsSystem/system.logs";
+import {AuthBcrypt} from "../../../auth/bcrypt";
 
 @Injectable()
 export class CreateUserService {
 
-    constructor(private readonly userRepository: UsersRepository) {}
+    constructor(private readonly userRepository: UsersRepository) {
+    }
 
     public async execute(date_user: CreateUserDto) {
         const userAlreadyExists = await this.userRepository.getByCredential(date_user.email);
@@ -17,6 +19,8 @@ export class CreateUserService {
             throw new CustomerException("User Not Found", HttpStatus.NOT_FOUND)
         }
 
-        return this.userRepository.createUser(date_user);
+        const encodedPassword = await AuthBcrypt.hashPassword(date_user.password)
+
+        return this.userRepository.createUser({...date_user, password: encodedPassword});
     }
 }
